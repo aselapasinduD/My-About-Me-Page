0359eb3c36a8583e254c2920123dd152d1f21492
aselapasinduD <asela.pasindu.dias17@gmail.com>
2023-03-31 20:16:46 +0530

    I start to create Skills section. not done yet.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const Skills = () => {
 	
 	return (
-		<h1>Skills</h1>
+		<div class="skillsSection">
+			<h1>Skills</h1>
+			<div class="skillsList">
+				<div class="skill">
+					<h2>Programming</h2>
+				</div>
+			</div>
+		</div>
 	)
 }
 
