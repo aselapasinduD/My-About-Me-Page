b249d503a7641e501b519f536255d6af2584bd12
Asela Pasindu <118802942+aselapasinduD@users.noreply.github.com>
2023-04-28 11:13:57 -0700

    Revert "Revert pull to change Scroll navbar againg"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Linkedin, GitHub, WhatsApp, Instagram } from '../assets/social icons/social-icons.js';
 import navbar_background from '../assets/images/navbar_background.png'
 
-const NavBar = () => {
+const NavBar = (scroll) => {
+	{/*Navbar navigation varibals for create animation*/}
 	const [isItemsMove, setIsItemsMove] = useState(false);
 	const [isIconMove, setIsIconMove] = useState(false);
-
-	const [loopNum, setLoopNum] = useState(0);
-	const [isDeleting, setIsDeleting] = useState(false);
-	const toRotate = [ "Programmer", "Web Developer", "Web Designer", "UI/UX Designer"];
-	const [text, setText] = useState('');
-	const [delta, setDelta] = useState(300 - Math.random() * 100);
-	const period = 2000;
-
 	const handleClick = (event) => {
 		const clickedElementID = event.target.id;
 		let clickedElementClass = event.target.closest('ul').className;
@@ -38,6 +31,24 @@ const NavBar = () => {
 		}
 	}
 
+	{/*If Scroll Down to other sections will pop up Little navbar on the top of the window*/}
+	const [isNavbarScroll, setNavbarScroll] = useState(false);
+	const fixedNavbar = () => {
+		if (window.scrollY > 650){
+			setNavbarScroll(true);
+		} else {
+			setNavbarScroll(false);
+		}
+	}
+	window.addEventListener("scroll", fixedNavbar);
+
+	{/*Typing animation in Navbar Section*/}
+	const [loopNum, setLoopNum] = useState(0);
+	const [isDeleting, setIsDeleting] = useState(false);
+	const toRotate = [ "Programmer", "Web Developer", "Web Designer", "UI/UX Designer"];
+	const [text, setText] = useState('');
+	const [delta, setDelta] = useState(300 - Math.random() * 100);
+	const period = 2000;
 	useEffect(() => {
 		let ticker = setInterval(() => {
 			tick();
@@ -45,7 +56,6 @@ const NavBar = () => {
 		return () => {
 			clearInterval(ticker)};
 			}, [text])
-
 	const tick = () => {
 		let i = loopNum % toRotate.length;
 		let fullText = toRotate[i];
@@ -55,7 +65,6 @@ const NavBar = () => {
 
 		if(isDeleting) {
 			setDelta(prevDelta => prevDelta / 2);
-			console.log("run", loopNum);
 		}
 		if (!isDeleting && updatedText === fullText) {
 			setIsDeleting(true);
@@ -68,28 +77,28 @@ const NavBar = () => {
 	}
 
 	return (
-	<nav class="navbar">
+	<nav className="navbar">
 		<h2>Welcome to my portfolio</h2>
-		<h1>{"Hi! I'm Asela "}<span class="wrap">{text}</span></h1>
-		<div class="container">
-			<div class="sections-bar-nav">
+		<h1>{"Hi! I'm Asela "}<span className="wrap">{text}</span></h1>
+		<div className={`container ${isNavbarScroll === true ? "NavbarScroll": ""}`}>
+			<div className={`sections-bar-nav ${isNavbarScroll === true ? "scrollTop" : ""}`}>
 				<ul className="sections-bar">
-					<li className="nav-Items" ><a id="About_me" onMouseOver={handleClick} onClick={handleClick} href="#aboutMe" class="hover-effect">About Me</a></li>
-					<li className="nav-Items" ><a id="Skills" onMouseOver={handleClick} onClick={handleClick} href="#skills" class="hover-effect">Skills</a></li>
-					<li className="nav-Items"><a id="Current_projects" onMouseOver={handleClick} onClick={handleClick} href="#currentProject" class="hover-effect">Current <br /> Projects</a></li>
-					<div class="second-wrapper">
+					<li className="nav-Items" ><a id="About_me" onMouseOver={handleClick} onClick={handleClick} href="#aboutMe" className="hover-effect">About Me</a></li>
+					<li className="nav-Items" ><a id="Skills" onMouseOver={handleClick} onClick={handleClick} href="#skills" className="hover-effect">Skills</a></li>
+					<li className="nav-Items"><a id="Current_projects" onMouseOver={handleClick} onClick={handleClick} href="#currentProject" className="hover-effect">Current <br /> Projects</a></li>
+					<div className="second-wrapper">
 						<span className={`select-nav-Items ${isItemsMove === true ? "move-select-Items" : isItemsMove === "select3" ? "move-select-Items3" : null }`}></span>
 						<span className="Items-white select-nav-Items"></span>
 					</div>
 				</ul>
 			</div>
-			<div class="sections-bar-social">
+			<div className={`sections-bar-social ${isNavbarScroll === true ? "scrollTop" : ""}`}>
 				<ul className="sections-bar-icon">
-					<li className="social-Icon Linkedin"><a href="#"><img id="Linkedin" onMouseOver={handleClick} onClick={handleClick} class="svg-color hover-effect" src={Linkedin} alt="Linkedin" /></a></li>
-					<li className="social-Icon GitHub"><a href="#"><img id="GitHub" onMouseOver={handleClick} onClick={handleClick} class="svg-color hover-effect" src={GitHub} alt="GitHub" /></a></li>
-					<li className="social-Icon WhatsApp"><a href="#"><img id="WhatsApp" onMouseOver={handleClick} onClick={handleClick} class="svg-color hover-effect" src={WhatsApp} alt="WhatsApp" /></a></li>
-					<li className="social-Icon Instagram"><a href="#"><img id="Instagram" onMouseOver={handleClick} onClick={handleClick} class="svg-color hover-effect" src={Instagram} alt="Instagram" /></a></li>
-					<div class="second-wrapper">
+					<li className="social-Icon Linkedin"><a href="https://www.linkedin.com/in/asela-pasindu-dias-55a7a7210/" target="blank"><img id="Linkedin" onMouseOver={handleClick} onClick={handleClick} class="svg-color hover-effect" src={Linkedin} alt="Linkedin" /></a></li>
+					<li className="social-Icon GitHub"><a href="https://github.com/aselapasinduD" target="blank"><img id="GitHub" onMouseOver={handleClick} onClick={handleClick} class="svg-color hover-effect" src={GitHub} alt="GitHub" /></a></li>
+					<li className="social-Icon WhatsApp"><a href="https://wa.me/94789991578" target="blank"><img id="WhatsApp" onMouseOver={handleClick} onClick={handleClick} class="svg-color hover-effect" src={WhatsApp} alt="WhatsApp" /></a></li>
+					<li className="social-Icon Instagram"><a href="https://www.instagram.com/asel_pasindu/" target="blank"><img id="Instagram" onMouseOver={handleClick} onClick={handleClick} class="svg-color hover-effect" src={Instagram} alt="Instagram" /></a></li>
+					<div className="second-wrapper">
 						<span className={`select-social-Icon ${isIconMove === "Linkedin" ? "select-social-Icon" : isIconMove === "Github" ? "move-social-Icon" : isIconMove === "Whatsapp" ? "move-social-Icon3" : isIconMove === "Instagram" ? "move-social-Icon4" : null }`}></span>
 						<span className="Icon-white select-social-Icon"></span>
 					</div>
