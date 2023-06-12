d3ade960adea79e03b1149f078f02330b1239218
Asel Pasindu <118802942+aselapasinduD@users.noreply.github.com>
2023-06-11 18:15:35 -0700

    vite file changes

--- src/components/About_me.jsx
+++ src/components/About_me.jsx
@@ -10,7 +10,7 @@ const About_me = () => {
 				<img src={about_me} alt="About Me Page" />
 				<span>
 					<h2>Hello there</h2>
-					<p>My name is Asela, and I own VEBDRAKstudio. I have a wealth of web development experience. You may see some of the web development projects I worked on under the section titled "Current Projects." and I'm determined to develop an amazing website for you.</p>
+					<p>I'm Asela, and I own VEBDRAKstudio. I have a wealth of web development experience. You may see some of the web development projects I worked on under the section titled "Current Projects." and I'm determined to develop an amazing website for you.</p>
 				</span>
 			</section>
 			<section class="stories">
